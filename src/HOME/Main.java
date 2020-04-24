adeaafd549b60d37e22167f25c9a6ceaefb45095
NetBRAINdel <62387102+NetBRAINdel@users.noreply.github.com>
2020-04-24 13:57:18 +0300

    .

--- src/HOME/Main.java
+++ src/HOME/Main.java
@@ -3,22 +3,32 @@ package HOME;
 import java.util.InputMismatchException;
 import java.util.Scanner;
 
-public class Main {
-    public static void main(String[] args) {
+    public class Main {
+        public static void main(String[] args) {
             int q = 0;
             int w = 3;
+            int a = 0;
             Scanner in = new Scanner(System.in);
             System.out.println("Введите число от 1 до 10");
             System.out.println("у вас " + w + " попытки");
 
             for (int i = 0; i < 3; i++) {
 
+//__________________________________________ПРОВЕРКА ОШИБКИ(ТЕКСТ)___________________________________________________\\
                 try {
-                    int a = in.nextInt();
-                System.out.println("ваше число: " + a);
+                    a = in.nextInt();
                     q++;
                     w--;
-
+                    System.out.println("ваше число: " + a);
+                }catch (InputMismatchException e){      //InputMismatchException\\---ОШИБКА ВВОДА ТЕКСТА.
+                    q++;
+                    w--;
+                    //System.err.println(); //выделить красным\\
+                    System.out.println("ввод текста не допустим, попыток осталось: "+"|"+w+"|" );
+                    in.next();
+                }
+//___________________________________________________________________________________________________________________\\
+//____________________________ПРОВЕРКА УСЛОВИЙ(ДИАПОЗОН ОТ 1 ДО 10(ВКЛЮЧИТЕЛЬНО),(3 ПОПЫТКИ))________________________\\
                 if (a > 10 || a < 1) {
                     System.out.println("оно не соответствует условию выше(ЧИСЛО от 1 до 10)...");
 
@@ -34,15 +44,9 @@ public class Main {
                     System.out.println("ПОКА!");
                     System.exit(0);
                 }
-                }catch (InputMismatchException e){
-                    w--;
-                    System.out.println("ввод текста не допустим, попыток осталось: "+"|"+w+"|" );
-
-
-                    in.next();
-                }
+//___________________________________________________________________________________________________________________\\
             }
+        }
     }
-}
 
 
